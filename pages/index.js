9df40809f81ca35405bc6a2dcb7154fe0f618cc0
jonas-hurst <jonhur@web.de>
2021-11-15 12:52:10 +0100

    added style to why join box

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ function SignupButton() {
 
 function WhyJoin() {
   return (
-    <div>
+    <div className={styles.whyJoin}>
       <h2>Why Join?</h2>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
       eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
