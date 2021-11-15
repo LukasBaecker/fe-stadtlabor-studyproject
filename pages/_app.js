03e5faa56f7ffafec2a6413fa0db7267f897bfb0
jonas-hurst <jonhur@web.de>
2021-11-15 12:36:03 +0100

    Edit page title and viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/globals.scss";
+import Head from "next/head";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <title>GardenUp!</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Component {...pageProps} />;
+    </React.Fragment>
+  );
 }
 
 export default MyApp;
