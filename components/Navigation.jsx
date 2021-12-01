8d4aaba07a41bc670454b89a4c968f5fac527656
LukasBaecker <baecker.lukas@outlook.de>
2021-12-01 19:36:37 +0100

    navigation menu

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,15 +1,29 @@
 import Link from "next/link";
-import styles from "../styles/Navigation.module.scss";
 
 function Navigation() {
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li>
-          <Link href='/signIn'>Sign In</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <div className='navigation-wrap'>
+        <input type='checkbox' className='navigation-toggler' />
+        <div className='hamburger'>
+          <div></div>
+        </div>
+        <div className='menu'>
+          <div>
+            <div>
+              <ul>
+                <li>
+                  <Link href='/signIn'>Sign In</Link>
+                </li>
+                <li>
+                  <Link href='/user'>Userpage</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
