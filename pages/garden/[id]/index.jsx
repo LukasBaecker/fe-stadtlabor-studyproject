f057d4bc142e7141bf722e3936d2e4eb067a3e57
jonas-hurst <jonhur@web.de>
2021-12-07 17:40:46 +0100

    added button group

--- pages/garden/[id]/index.jsx
+++ pages/garden/[id]/index.jsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -15,6 +17,13 @@ function garden() {
 
   const [gardenName, setGardenName] = useState("Münstergarden");
 
+  // controls, what the page is showing, depending on which button is pressed
+  // 1: Info (default, shows when site is loaded)
+  // 2: Events
+  // 3: Members
+  // 4: Sharables
+  const [pageState, setPageState] = useState(1);
+
   return (
     <>
       <Head>
@@ -27,6 +36,24 @@ function garden() {
         name={gardenName}
         imgUrl="/imgs/markus-spiske-bk11wZwb9F4-unsplash-square.jpg"
       />
+
+      {/* Page Content */}
+      <div className={styles.Content}>
+        <ButtonGroup aria-label="Basic example" className={styles.buttonGroup}>
+          <Button variant="primary" onClick={() => setPageState(1)}>
+            Info
+          </Button>
+          <Button variant="primary" onClick={() => setPageState(2)}>
+            Events
+          </Button>
+          <Button variant="primary" onClick={() => setPageState(3)}>
+            Members
+          </Button>
+          <Button variant="primary" onClick={() => setPageState(4)}>
+            Sharables
+          </Button>
+        </ButtonGroup>
+      </div>
     </>
   );
 }
